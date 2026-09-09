221ce0d2cd7816a9fafde4562f7feb2783fca0da
Burt du Preez <fusionhs@hotmail.com>
2026-09-09 21:38:32 +0200

    Fix nvim setup

--- .config/nvim/lua/fusionhs/lazy.lua
+++ .config/nvim/lua/fusionhs/lazy.lua
@@ -1,13 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazy_commit = "bef521ac89c8d423f9d092e37b58e8af0c099309"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
     lazypath,
   })
+  vim.fn.system({ "git", "-C", lazypath, "checkout", "--detach", lazy_commit })
 end
 vim.opt.rtp:prepend(lazypath)
 
